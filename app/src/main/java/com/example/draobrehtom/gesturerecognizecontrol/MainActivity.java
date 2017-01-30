273889efccf8c6c5ef1a90ca79b02083fc75e7e7
Daniel Kabanov <draobrehtom@gmail.com>
2017-01-30 22:55:00 +0100

    Add OpenCV support

--- app/src/main/java/com/example/draobrehtom/gesturerecognizecontrol/MainActivity.java
+++ app/src/main/java/com/example/draobrehtom/gesturerecognizecontrol/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.draobrehtom.gesturerecognizecontrol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    static {
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV not Loaded");
+        } else {
+            Log.d(TAG, "OpenCV Loaded");
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
